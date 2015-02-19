f76c4f0f38ad8d534aa468d2a9eb7e9ec58d4c6c
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-02-19 16:55:24 +0100

    Revert "Revert "add savefiledialog""
    This reverts commit 88bdb262194b7d01a2e82deec41a7f3592b7b638.

--- ExcelCalendar/ExcelCalendarForm.Designer.cs
+++ ExcelCalendar/ExcelCalendarForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExcelCalendarForm));
             this.generateExcel = new System.Windows.Forms.Button();
+            this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.saveExcel = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayout.SuspendLayout();
             this.SuspendLayout();
             // 
             // generateExcel
@@ -38,13 +41,25 @@
             this.generateExcel.Name = "generateExcel";
             this.generateExcel.UseVisualStyleBackColor = true;
             // 
+            // tableLayout
+            // 
+            resources.ApplyResources(this.tableLayout, "tableLayout");
+            this.tableLayout.Controls.Add(this.generateExcel, 0, 1);
+            this.tableLayout.Name = "tableLayout";
+            // 
+            // saveExcel
+            // 
+            resources.ApplyResources(this.saveExcel, "saveExcel");
+            this.saveExcel.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
             // ExcelCalendarForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.generateExcel);
+            this.Controls.Add(this.tableLayout);
             this.Name = "ExcelCalendarForm";
             this.Load += new System.EventHandler(this.ExcelCalendarForm_Load);
+            this.tableLayout.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -52,6 +67,8 @@
         #endregion
 
         private System.Windows.Forms.Button generateExcel;
+        private System.Windows.Forms.TableLayoutPanel tableLayout;
+        private System.Windows.Forms.SaveFileDialog saveExcel;
     }
 }
 
