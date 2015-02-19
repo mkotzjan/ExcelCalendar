06d8948be5aaf0c1530d13cfe0c24a8b48743784
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-02-19 16:12:52 +0100

    Add German language ressource file

--- ExcelCalendar/ExcelCalendarForm.Designer.cs
+++ ExcelCalendar/ExcelCalendarForm.Designer.cs
@@ -28,21 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExcelCalendarForm));
+            this.generateExcel = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // generateExcel
+            // 
+            resources.ApplyResources(this.generateExcel, "generateExcel");
+            this.generateExcel.Name = "generateExcel";
+            this.generateExcel.UseVisualStyleBackColor = true;
             // 
             // ExcelCalendarForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.generateExcel);
             this.Name = "ExcelCalendarForm";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.ExcelCalendarForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button generateExcel;
     }
 }
 
