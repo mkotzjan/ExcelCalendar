2a3b55b0344284330da4b79f3e835125d7bd8c51
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-03-31 13:37:28 +0200

    created methode names

--- ExcelCalendar/generateOpen.cs
+++ ExcelCalendar/generateOpen.cs
@@ -7,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace ExcelCalendar
 {
-    public class generateOpen : IGenerate
+    public class GenerateOpen : IGenerate
     {
+        public GenerateOpen(string filePath, List<IPerson> persons)
+        {
+
+        }
+
+//        private void releaseObject(object obj)
+//        {
+
+//        }
+
+//        private void setTitle(Excel.Worksheet xlWorkSheet)
+//        {
+
+//        }
+
+//        private void setMonths(Excel.Worksheet xlWorkSheet, int i)
+//        {
+
+//        }
+
+//        private void setBorders(Excel.Worksheet xlWorkSheet, int i, int j)
+//        {
+
+//        }
+
+//        private void setDaysOfMonth(Excel.Worksheet xlWorkSheet, int i, int j)
+//        {
+
+//        }
+
+//        private void setFeastDays(Excel.Worksheet xlWorkSheet, int i, int j)
+//        {
+
+//        }
+
+//        private void setHolidays(Excel.Worksheet xlWorkSheet, int i, int j)
+//        {
+
+//        }
+
+//        private void setWeek(Excel.Worksheet xlWorkSheet, int i, int j)
+//        { 
+
+//        }
     }
 }
